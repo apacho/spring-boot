5a1b1ce49c949894580ad8c27f03ff283af068f6
Anupesh Pachori <pachori.anupesh@gmail.com>
2020-06-11 11:32:41 +0530

    Update data.sql

--- spring-boot-hibernate-crud-demo/src/main/resources/data.sql
+++ spring-boot-hibernate-crud-demo/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO 
 	TBL_EMPLOYEES (first_name, last_name, email) 
 VALUES
-  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com'),
-  	('John', 'Doe', 'xyz@email.com');
+  	('Anupesh', 'Pachori', 'a@gmail.com'),
+  	('John', 'Dwone', 'xyz@email.com');
